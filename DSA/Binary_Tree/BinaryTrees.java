8c1e105c11ba3c7fcdb7405aa3a125e3e537ff5a
Jaykant-yadav <jaykant.yadav.cse@gmail.com>
2025-05-06 23:51:34 +0530

    Sum of nodes

--- DSA/Binary_Tree/BinaryTrees.java
+++ DSA/Binary_Tree/BinaryTrees.java
@@ -89,18 +89,68 @@ public class BinaryTrees {
         }
     }
 
+    // Height of the tree
+    public static int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    // Count Of Node
+    public static int count(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftCount = count(root.left);
+        int rightCount = count(root.right);
+        return leftCount + rightCount + 1;
+    }
+
+    // Sum of Nodes
+    public static int sumOfNodes(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
     public static void main(String[] args) {
-        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        /* int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(nodes);
+        Node root = tree.buildTree(nodes); */
         // System.out.println(root.data);
 
-        preOrder(root);
+        /* preOrder(root);
         System.out.println();
         InOrder(root);
         System.out.println();
         postOrder(root);
         System.out.println();
-        levelOrder(root);
+        levelOrder(root); */
+
+        /*              1
+         *            /   \
+         *           2     3
+         *          / \   / \
+         *         4   5 6   7
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(height(root));
+        System.out.println(count(root));
+        System.out.println(sumOfNodes(root));
     }
 }

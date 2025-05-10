484a632134b65e4189c602a8610cfc8d49be04de
Jaykant-yadav <jaykant.yadav.cse@gmail.com>
2025-05-10 11:34:36 +0530

    Min Distance b/w nodes

--- DSA/Binary_Tree/BinaryTrees.java
+++ DSA/Binary_Tree/BinaryTrees.java
@@ -255,38 +255,39 @@ public class BinaryTrees {
 
     // Kth Level Of a Tree
     public static void KLevel(Node root, int level, int k) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
 
-        if(level == k) {
+        if (level == k) {
             System.out.print(root.data + " ");
         }
 
-        KLevel(root.left, level+1, k);
-        KLevel(root.right, level+1, k);
+        KLevel(root.left, level + 1, k);
+        KLevel(root.right, level + 1, k);
     }
 
     // Lowest Common Ancestor
+    // Approach - 1
     public static boolean getPath(Node root, int n, ArrayList<Node> path) {
-        if(root == null) {
+        if (root == null) {
             return false;
         }
 
         path.add(root);
 
-        if(root.data == n) {
+        if (root.data == n) {
             return true;
         }
 
         boolean foundLeft = getPath(root.left, n, path);
         boolean foundRight = getPath(root.right, n, path);
 
-        if(foundLeft || foundRight) {
+        if (foundLeft || foundRight) {
             return true;
         }
 
-        path.remove(path.size()-1);
+        path.remove(path.size() - 1);
         return false;
     }
 
@@ -299,16 +300,67 @@ public class BinaryTrees {
 
         // Last common ancestor
         int i = 0;
-        for(; i<path1.size() && i<path2.size(); i++) {
-            if(path1.get(i) != path2.get(i)) {
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
                 break;
             }
         }
 
         // last equal node -> i-1th
-        Node lca = path1.get(i-1);
+        Node lca = path1.get(i - 1);
         return lca;
     }
+
+    // Approach - 2
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        // LeftLCA = val rightLca = null
+        if (rightLca == null) {
+            return leftLca;
+        }
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+
+    // Min Distance between nodes
+    public static int lcaDist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
     public static void main(String[] args) {
         /*
          * int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -362,6 +414,8 @@ public class BinaryTrees {
         // KLevel(root, 1, 3);
 
         int n1 = 4, n2 = 5;
-        System.out.println(lca(root, n1, n2).data);
+        // System.out.println(lca(root, n1, n2).data);
+        // System.out.println(lca2(root, n1, n2).data);
+        System.out.println(minDist(root, n1, n2));
     }
 }

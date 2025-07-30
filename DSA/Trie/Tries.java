959c161d2f40923896e6b980bc1a644ca3b864f4
Jaykant Kumar <jaykant.yadav.cse@gmail.com>
2025-07-31 00:13:42 +0530

    Trie Data Structure

---
+++ DSA/Trie/Tries.java
@@ -0,0 +1,19 @@
+package DSA.Trie;
+
+public class Tries {
+    static class Node{
+        Node[] children = new Node[26];
+        boolean endOfWord = false;
+
+        Node() {
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+    public static void main(String[] args) {
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+    }
+}

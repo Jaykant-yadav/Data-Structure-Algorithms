ad80b987e17e0b59c947acb396d957ebca7d8a08
Jaykant Kumar <jaykant.yadav.cse@gmail.com>
2025-07-30 23:30:33 +0530

    SubArray sum equal to k

--- DSA/Hashing/HashingQs.java
+++ DSA/Hashing/HashingQs.java
@@ -146,6 +146,26 @@ public class HashingQs {
 
         System.out.println("largest subArray with sum as 0 => " + len);
     }
+
+    // SubArray sum equal to k
+    public static void subArraySumEqltoK(int nums[], int k){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // (Sum, count)
+        map.put(0, 1);
+
+        int sum = 0;
+        int ans = 0;
+
+        for(int j=0; j<nums.length; j++){ // O(n)
+            sum += nums[j]; //sum(j)
+            if(map.containsKey(sum - k)){
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        System.out.println(ans);
+    }
     
     public static void main(String[] args) {
         String s = "race";
@@ -198,6 +218,11 @@ public class HashingQs {
         // Largest SubArray with 0 Sum
         int num[] = {15, -2, 2, -8, 1, 7, 10, 23};
         largestSubArrayWithZeroSum(num);
+
+
+        // SubArray sum equal to k
+        int array[] = {10, 2, -2, -20, 10};
+        subArraySumEqltoK(array, -10);
 
 
     }

9353985839f735e87baf2b7c858e1d741e137ed1
Jaykant Kumar <jaykant.yadav.cse@gmail.com>
2025-07-31 16:10:08 +0530

    Trie operation like search, insertion, creatation

--- DSA/Trie/Tries.java
+++ DSA/Trie/Tries.java
@@ -13,7 +13,44 @@ public class Tries {
     }
 
     public static Node root = new Node();
+
+    // Insert
+    public static void insert(String word){ //O(L)
+        Node curr = root;
+        for(int level = 0; level < word.length(); level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.endOfWord = true;
+    }
+
+    // Search
+    public static boolean search(String key){ //O(L)
+        Node curr = root;
+        for(int level = 0; level < key.length(); level++){
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+
+        return curr.endOfWord == true;
+    }
+
     public static void main(String[] args) {
         String words[] = {"the", "a", "there", "their", "any", "thee"};
+        for(int i=0; i<words.length; i++){
+            insert(words[i]);
+        }
+
+        System.out.println(search("thee")); //true
+        System.out.println(search("theor"));//false
     }
 }
